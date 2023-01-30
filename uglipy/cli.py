382f8ef4afe023be4cc3c37bb029b6a488e864e8
Alvin Wan <alvinwan1@gmail.com>
2023-01-29 23:32:37 -0800

    more proper cli

--- uglipy/cli.py
+++ uglipy/cli.py
@@ -2,10 +2,16 @@ import glob
 import sys
 from pathlib import Path
 from uglipy.ugli import uglipy
+from argparse import ArgumentParser
 
 def main():
+    parser = ArgumentParser()
+    parser.add_argument('path', help='Path to the file or directory to uglipy')
+    parser.add_argument('-o', '--output', help='Path to the output directory', default='./')
+    args = parser.parse_args()
+
     sources, modules = [], []
-    for path in glob.iglob(sys.argv[1]):
+    for path in glob.iglob(args.path):
         if not path.endswith('.py') or '.ugli.' in path:
             continue
         with open(path) as f:
@@ -13,7 +19,7 @@ def main():
         modules.append(Path(path).stem)
     cleaned, modules = uglipy(sources, modules)
     for source, module in zip(cleaned, modules):
-        with open(f'out/{module}.ugli.py', 'w') as f:
+        with open(Path(args.output) / f'{module}.ugli.py', 'w') as f:
             f.write(source)
 
 
