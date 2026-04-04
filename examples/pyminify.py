34b6c9570968a31818fc0dd3f4b68572d8d8eefe
Alvin Wan <alvinwan@users.noreply.github.com>
2026-04-04 05:26:26 -0700

    Reintroduce local string hoisting (#8)
    * reintroduce local string hoisting
    
    * guard pattern AST nodes on Python 3.9
    
    * avoid hoist helper collisions

--- examples/pyminify.py
+++ examples/pyminify.py
@@ -1,22 +1,22 @@
 def a(event,context):
- l.info(event)
+ f='RequestType';g='PhysicalResourceId';h='None';i='Status';j='SUCCESS';k='Tags';m='OldResourceProperties';l.info(event)
  try:
   b=hashlib.new('md5',(event['RequestId']+event['StackId']).encode()).hexdigest();c=event['ResourceProperties']
-  if event['RequestType']=='Create':
-   event['PhysicalResourceId']='None';event['PhysicalResourceId']=create_cert(c,b);add_tags(event['PhysicalResourceId'],c);validate(event['PhysicalResourceId'],c)
-   if wait_for_issuance(event['PhysicalResourceId'],context):event['Status']='SUCCESS';return send(event)
+  if event[f]=='Create':
+   event[g]=h;event[g]=create_cert(c,b);add_tags(event[g],c);validate(event[g],c)
+   if wait_for_issuance(event[g],context):event[i]=j;return send(event)
    else:return reinvoke(event,context)
-  elif event['RequestType']=='Delete':
-   if event['PhysicalResourceId']!='None':acm.delete_certificate(CertificateArn=event['PhysicalResourceId'])
-   event['Status']='SUCCESS';return send(event)
-  elif event['RequestType']=='Update':
+  elif event[f]=='Delete':
+   if event[g]!=h:acm.delete_certificate(CertificateArn=event[g])
+   event[i]=j;return send(event)
+  elif event[f]=='Update':
    if replace_cert(event):
-    event['PhysicalResourceId']=create_cert(c,b);add_tags(event['PhysicalResourceId'],c);validate(event['PhysicalResourceId'],c)
-    if not wait_for_issuance(event['PhysicalResourceId'],context):return reinvoke(event,context)
+    event[g]=create_cert(c,b);add_tags(event[g],c);validate(event[g],c)
+    if not wait_for_issuance(event[g],context):return reinvoke(event,context)
    else:
-    if 'Tags' in event['OldResourceProperties']:acm.remove_tags_from_certificate(CertificateArn=event['PhysicalResourceId'],Tags=event['OldResourceProperties']['Tags'])
-    add_tags(event['PhysicalResourceId'],c)
-   event['Status']='SUCCESS';return send(event)
+    if k in event[m]:acm.remove_tags_from_certificate(CertificateArn=event[g],Tags=event[m][k])
+    add_tags(event[g],c)
+   event[i]=j;return send(event)
   else:raise RuntimeError('Unknown RequestType')
- except Exception as d:l.exception('');event['Status']='FAILED';event['Reason']=str(d);return send(event)
+ except Exception as d:l.exception('');event[i]='FAILED';event['Reason']=str(d);return send(event)
 handler=a
